1ba88418e2418b3a7d88d1ff74fbe6e07c83cfef
Blake Burkett <blake.a.burkett@gmail.com>
2017-05-03 19:36:15 -0400

    Cleaning up commands

--- yar.py
+++ yar.py
@@ -188,10 +188,11 @@ if __name__ == '__main__':
     parser.add_argument('-v', help='verbose', action='store_true')
     parser.add_argument('-i', help='input', action='store_true')
     parser.add_argument('-o', help='output', action='store_true')
-    parser.add_argument('--subreddit', help='The subreddit you want to learn on.')
+    parser.add_argument('subreddit', help='The subreddit you want to learn on.')
     parser.add_argument('--pages', help='Number of pages to traverse. Default is 2', default=2, type=int)
     parser.add_argument('--comments', help='Number of comments to generate. Default is 1', default=1, type=int)
     args = parser.parse_args()
+    print(args)
     try:
         main(args)
     except KeyboardInterrupt:
